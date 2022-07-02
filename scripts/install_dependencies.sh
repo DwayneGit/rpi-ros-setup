d2e3464b12ab4a4c01b4c4295259ad56963edcd3
Dwayne Williams <dewilliams0327@gmail.com>
2022-07-02 17:43:09 -0500

    add pigpio and pico to setup

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,16 @@
 # install
-sudo apt install curl wget python3-pip git gcc g++ make
+sudo apt install curl wget python3-pip git gcc g++ make python-setuptools python3-setuptools
+
+wget https://github.com/joan2937/pigpio/archive/master.zip
+unzip master.zip
+cd pigpio-master
+make
+sudo make install
+
+sudo cp ../files/pigpiod.service /lib/systemd/system/pigpiod.service
+sudo systemctl start pigpiod.service
+sudo systemctl enable pigpiod.service
+
+wget https://raw.githubusercontent.com/raspberrypi/pico-setup/master/pico_setup.sh
+sudo chmod _x pico_setup.sh
+./pico_setup.sh
