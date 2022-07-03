c9ae9fb3b94211e3c6b35207b8b63a08d6d26852
Dwayne Williams <dewilliams0327@gmail.com>
2022-07-02 21:31:19 -0500

    change node version

--- scripts/setup_code_server.sh
+++ scripts/setup_code_server.sh
@@ -1,13 +1,14 @@
 # install vs code
 
 # install node js & yarn
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm -g install yarn
 
 yarn global add code-server 
 
-sudo cp ../files/config.yaml ~/.config/code-server/config.yaml
+mkdir ~/.config/code-server
+cp ../files/config.yaml ~/.config/code-server/config.yaml
 
 sudo cp ../files/code-server.service /etc/systemd/system/code-server.service
 sudo systemctl start code-server
